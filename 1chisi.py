d488583f4f5ddf980f392f6f4b52628304acbb27
Mokhirjon <Mokhirjon@test.com>
2023-07-16 20:57:52 +0500

    first time

---
+++ 1chisi.py
@@ -0,0 +1,9 @@
+akbarjon =int(input('akbarjon'))
+olimxon=int(input('olimxon'))
+try:
+     if akbarjon>olimxon:
+         print('akbarjon', akbarjon)
+     else:
+         print("olimxon", olimxon)
+except:
+     print('Error')
